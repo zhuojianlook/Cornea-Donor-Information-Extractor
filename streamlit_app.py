7acda9294c8a4cbcc1e263074bbc6a5233fad343
zhuojianlook <117797221+zhuojianlook@users.noreply.github.com>
2023-10-20 15:20:42 +0800

    sa

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,16 @@ def streamlit_app():
         extracted_data_list = []
 
         for uploaded_file in uploaded_files:
-        # ... [other code]
-        extracted_values = heuristic_extract_values_from_text(extracted_text)  # Use this function instead
-        extracted_data_list.append(extracted_values)
+            # Read the uploaded file's content into a BytesIO stream
+            file_content = io.BytesIO(uploaded_file.read())
+            
+            layout_data = extract_layout_updated(file_content)
+            extracted_text = [entry["text"] for entry in layout_data]
+            
+            extracted_values = heuristic_extract_values_from_text(extracted_text)  # Use this function instead
+            extracted_data_list.append(extracted_values)
 
-    extracted_df = pd.DataFrame(extracted_data_list)
-    st.write(extracted_df)
-
+        extracted_df = pd.DataFrame(extracted_data_list)
+        st.write(extracted_df)
+        
 streamlit_app()
